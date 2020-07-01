26dd03b6cc1b2fef002beb91e6dbab37e5b2be75
Tim Liu <timothytcliu@gmail.com>
2020-07-01 16:58:02 -0700

    navigation part 1

--- components/CampsiteInfoComponents.js
+++ components/CampsiteInfoComponents.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Text, View, ImagePropTypes } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-const RenderCampsite = ({ campsite }) => {
+function RenderCampsite({ campsite }) {
   if (campsite) {
     return (
       <Card
@@ -14,10 +15,27 @@ const RenderCampsite = ({ campsite }) => {
     );
   }
   return <View />;
-};
+}
 
-const CampsiteInfo = props => {
-  return <RenderCampsite campsite={props.campsite} />;
-};
+class CampsiteInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  static navigationOptions = {
+    title: 'Campsite Information',
+  };
+
+  render() {
+    const campsiteId = this.props.navigation.getParam('campsiteId');
+    const campsite = this.state.campsites.filter(
+      campsite => campsite.id === campsiteId
+    )[0];
+    return <RenderCampsite campsite={campsite} />;
+  }
+}
 
 export default CampsiteInfo;
