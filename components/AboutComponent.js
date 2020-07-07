a4972f7f4d3d37e58f8cecd64daa1fc94facce87
Tim Liu <timothytcliu@gmail.com>
2020-07-07 11:38:16 -0700

    activity indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -42,6 +43,26 @@ class About extends Component {
       );
     };
 
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
         <Mission />
@@ -50,7 +71,7 @@ class About extends Component {
             data={this.props.partners.partners}
             renderItem={renderPartner}
             keyExtractor={item => item.id.toString()}
-          ></FlatList>
+          />
         </Card>
       </ScrollView>
     );
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -12,7 +13,19 @@ const mapStateToProps = state => {
   };
 };
 
-function RenderItem({ item }) {
+function RenderItem(props) {
+  const { item } = props;
+
+  if (props.isLoading) {
+    return <Loading />;
+  }
+  if (props.errMess) {
+    return (
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
+    );
+  }
   if (item) {
     return (
       <Card featuredTitle={item.name} image={{ uri: baseUrl + item.image }}>
@@ -37,6 +50,8 @@ class Home extends Component {
               campsite => campsite.featured
             )[0]
           }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
         />
         <RenderItem
           item={
@@ -44,11 +59,15 @@ class Home extends Component {
               promotion => promotion.featured
             )[0]
           }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
         />
         <RenderItem
           item={
             this.props.partners.partners.filter(partner => partner.featured)[0]
           }
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
         />
       </ScrollView>
     );
