f68870bc92e2a5b585a534fafa2115905534aaa3
Tim Liu <timothytcliu@gmail.com>
2020-07-06 17:29:58 -0700

    using redux

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Main from './components/MainComponents';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 export default function App() {
-  return <Main />;
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 }
--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+  return {
+    partners: state.partners,
+  };
+};
 
 const Mission = () => {
   return (
@@ -20,13 +27,6 @@ const Mission = () => {
 };
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      partners: PARTNERS,
-    };
-  }
-
   static navigationOptions = {
     title: 'About Us',
   };
@@ -37,7 +37,7 @@ class About extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
       );
     };
@@ -47,7 +47,7 @@ class About extends Component {
         <Mission />
         <Card title="Community Partners">
           <FlatList
-            data={this.state.partners}
+            data={this.props.partners.partners}
             renderItem={renderPartner}
             keyExtractor={item => item.id.toString()}
           ></FlatList>
@@ -57,4 +57,4 @@ class About extends Component {
   }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
