5187503b629b240cabb23c1709cf4e3cd25e241e
Tim Liu <timothytcliu@gmail.com>
2020-07-13 11:43:55 -0700

    week 2 assignment, and Favorites

--- redux/comments.js
+++ redux/comments.js
@@ -1,12 +1,31 @@
 import * as ActionTypes from './ActionTypes';
 
-export const comments = (state = { errMess: null, comments: [] }, action) => {
+export const comments = (
+  state = {
+    errMess: null,
+    comments: [],
+  },
+  action
+) => {
   switch (action.type) {
     case ActionTypes.ADD_COMMENTS:
       return { ...state, errMess: null, comments: action.payload };
 
     case ActionTypes.COMMENTS_FAILED:
       return { ...state, errMess: action.payload };
+
+    // case ActionTypes.ADD_COMMENT:
+    //   const comment = { ...action.payload, id: state.comments.length };
+    //   return {
+    //     ...state,
+    //     errMess: null,
+    //     comments: state.comments.concat(comment),
+    //   };
+
+    case ActionTypes.ADD_COMMENT:
+      const comment = { ...action.payload, id: state.comments.length };
+      const comments = [...state.comments, comment];
+      return { ...state, errMess: null, comments };
 
     default:
       return state;
